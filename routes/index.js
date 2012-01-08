728b3b53df0678d8b8d9bced42a9ee797ae5fa91
Chris Airey <cairey01@hotmail.com>
2012-01-08 23:10:44 +0000

    msg

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+exports.index = function(app){
+    
+    app.get('/articles/new', function(req, res){
+        res.render('index', { title: 'Express' });
+    });  
 };
